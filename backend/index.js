a98d4bbcb5e5ee667e22b1f4e5aeb851d12a991b
manoj kumar <manojtewatia9211@gmail.com>
2023-06-15 00:08:05 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:3000", // Allow requests from this specific origin
+  optionsSuccessStatus: 200, // Return 200 for preflight requests
+};
+
+server.use(cors(corsOptions));
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
